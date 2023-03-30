a5ba822f190549a4474f9fe3d44b88719b7b3859
klankowi <klankowicz@gmri.org>
2023-03-30 10:21:41 -0400

    Add method for updating depth
    Add code to merge station data with rasterized NOAA bathymetry (1 minute resolution, finest allowable) and select whether depth will be represented as data provided by interviewee or NOAA bathymetry.
    
    Add cleaned data with updated depth

---
+++ R_code/join_SST_OISST.R
@@ -0,0 +1,248 @@
+# Adapted from code by Sarah Gaichas
+# Joins existing field station SST data and OISST satellite SST data, tests
+# relationship between the two, visualizes OISST inputs at stations missing
+# empirical data.
+
+# Clear workspace
+rm(list=ls())
+
+# Load libraries
+library(tidyverse)
+library(here)
+library(DT)
+library(FishStatsUtils)
+library(sf)
+library(raster)
+library(terra)
+library(nngeo)
+library(data.table)
+
+# Set GGplot auto theme
+theme_set(theme(panel.grid.major = element_line(color='lightgray'),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(),
+                panel.border = element_rect(color='black', size=1, fill=NA),
+                legend.position = "bottom",
+                axis.text.x=element_text(size=12),
+                axis.text.y=element_text(size=12),
+                axis.title.x=element_text(size=14),
+                axis.title.y=element_text(size=14, angle=90, vjust=2),
+                plot.title=element_text(size=14, hjust = 0, vjust = 1.2),
+                plot.caption=element_text(hjust=0, face='italic', size=12)))
+
+
+#list of SST dataframes
+SSTdfs <- list.files(here("Data/OISST/"), pattern = "*.rds")
+
+# Create empty tibble to fill
+stn_OISST <- tibble()
+
+# Load station data
+# Load data
+dat.1993 <- read.csv(here('Data/Clean/LPS_Pre_2002_Clean.csv'))
+dat.2021 <- read.csv(here('Data/Clean/LPS_Post_2002_Clean.csv'))
+stations <- rbind(dat.1993, dat.2021)
+rm(dat.1993, dat.2021)
+
+# Remove large category (it's fine, it's a spatial duplicate of small.)
+stations <- subset(stations, Size_class == 'small')
+
+stations$DATE <- paste0(stations$year, '-',
+                        str_pad(stations$month, 2, "left", "0"), "-",
+                        str_pad(stations$day, 2, "left", '0'))
+
+# Create datestring to merge SST and OISST
+stations$DATE <- as.POSIXct(stations$DATE, 
+                            format = "%Y-%m-%d")
+stations$yrmody <- paste0(stations$year, 
+                          str_pad(stations$month, 2, "left", '0'), 
+                          str_pad(stations$day, 2, "left", '0'))
+stations <- st_as_sf(stations, coords=c("lon", "lat"),
+                     na.fail=T)
+#stations <- subset(stations, YEAR > 1981)
+
+# Initialize progress bar
+pb <- txtProgressBar(min=0, max=length(SSTdfs), initial=0, char="=", style=3)
+
+# Merge in year-based loops
+for(df in SSTdfs){
+  # Call the number of the current .rds file in the vector of .rds files
+  t <- match(df, SSTdfs)
+  
+  # Update progress bar
+  setTxtProgressBar(pb, t)
+  getTxtProgressBar(pb)
+  
+  # Call annual .rds OISST file
+  sstdf <- readRDS(paste0(here("Data/OISST/", df)))
+  sstdf$yrmody <- paste0(sstdf$year, sstdf$month, sstdf$day)
+  
+  # Create string of sampled years (removes cod data prior to 1982)
+  stationsyr <- stations %>%
+    filter(year == unique(sstdf$year))
+  
+  # Filter out OISST days not sampled for bluefin, convert to sf object
+  sstdf_survdays <- sstdf %>%
+    dplyr::mutate(yrmody = as.numeric(paste0(year, month, day)) )%>%
+    dplyr::filter(yrmody %in% unique(stationsyr$yrmody)) %>%
+    dplyr::mutate(year = as.numeric(year),
+                  month = as.numeric(month),
+                  day = as.numeric(day),
+                  declon = Lon,
+                  declat = Lat) %>%
+    dplyr::select(-Lon, -Lat) %>%
+    sf::st_as_sf(coords=c("declon","declat"), crs=4326, remove=FALSE)  
+  
+  # Join by nearest neighbor and date
+  yrdietOISST <- suppressMessages(do.call('rbind', 
+                         lapply(split(stationsyr, 
+                                      1:nrow(stationsyr)), 
+                  function(x) {
+                        st_join(x, 
+                                sstdf_survdays[sstdf_survdays$yrmody == unique(x$yrmody),],
+                                join = st_nn, k = 1, progress = TRUE)
+                               }
+  )))
+  
+  # Bind one year of the loop to the initialized tibble
+  stn_OISST <- rbind(stn_OISST, yrdietOISST)
+  
+  # Close segment of progress bar every loop
+  close(pb)
+  
+}
+
+# Save output
+#saveRDS(stn_OISST, here("data/RData_Storage/stn_OISST.rds"))
+
+# Read in station data and station-OISST
+stn_OISST_merge <- stn_OISST %>%
+  dplyr::select(-month.y,
+                -day.y,
+                -yrmody.y) %>% 
+  dplyr::rename(month = month.x,
+                day = day.x,
+                yrmody = yrmody.x,
+                oisst = sst.y) %>%
+  dplyr::select(id, oisst) %>%
+  sf::st_drop_geometry()
+
+# Merge
+agg_stn_all_OISST <- left_join(stations, stn_OISST_merge)
+
+# Save output
+#saveRDS(agg_stn_all_OISST, here("Data/Clean/agg_stn_all_OISST.rds"))
+#agg_stn_all_OISST <- readRDS(here("data/agg_stn_all_OISST.rds"))
+
+# Create dataset of comparisons
+comparesst <- agg_stn_all_OISST %>%
+  #dplyr::filter(YEAR>1981)%>%
+  dplyr::select(sst, oisst, year) %>%
+  na.omit()
+
+# Set sequence of years to plot
+yearstoplot <- seq(1993, 2021)
+
+# Plot comparisons, check for adhesion to 1:1 line
+for(i in 1:length(yearstoplot)){
+  # Set year
+  plotyr <- yearstoplot[i]
+  
+  # Plot OISST vs Field SST for that year
+  print(ggplot2::ggplot(comparesst[comparesst$year == plotyr,], 
+                        aes(x=sst, y=oisst)) +
+    geom_point(alpha=0.8, pch=16)+
+    geom_abline(intercept = 0, slope = 1) +
+    labs(x = "Field surface temperature (deg C)",
+         y = "OISST (deg C)") +
+    theme_bw() +
+      ggtitle(paste("SST difference survey-OISST:", plotyr, sep = " ")))
+}
+
+# Plot OISST vs Field SST for all years
+ggplot2::ggplot(comparesst, aes(x=sst, y=oisst, col=year)) +
+  geom_point(alpha=0.8, pch=16)+
+  geom_abline(intercept = 0, slope = 1) +
+  labs(x = "Field surface temperature (deg C)",
+       y = "OISST (deg C)") +
+  theme_bw() 
+# Looks like we have mostly good field data, with the notable exception of 1993
+
+# Map annual shifts
+mapsst <- agg_stn_all_OISST %>%
+  #dplyr::filter(YEAR>1981) %>%
+  #dplyr::filter(SEASON == 'FALL' | SEASON=='SPRING') %>% 
+  dplyr::mutate(sstdiff = sst-oisst) %>%
+  dplyr::select(id, year, month, sst, oisst, sstdiff) 
+
+# Set CRS
+st_crs(mapsst) <- "EPSG:4326"
+
+# Set function to map SST difference
+yrmap <- function(mapyr){
+  coast <- ecodata::coast
+  coast <- st_transform(coast, crs="EPSG:4326")
+  ggplot2::ggplot() +
+    geom_sf(data = coast) +
+    geom_sf(data = mapsst[mapsst$year == mapyr,], 
+            aes(colour=sstdiff)) +
+    coord_sf(xlim = c(-77, -65), 
+             ylim = c(35, 45)) + 
+    scale_color_gradient2(low = "blue",
+                          mid = "gray",
+                          high = "red",
+                          midpoint = 0,
+                          limits=c(-25, 15),
+                          na.value = "gray") +
+    theme_bw() +
+    #facet_wrap(~month) +
+    ggtitle(paste("SST difference survey-OISST:", mapyr, sep = " "))
+}
+
+# Plot SST difference in space for every year
+for(mapyr in 1993:2021){
+  print(yrmap(mapyr)) 
+}
+
+# Executive decision: if field SST is within 2 deg of OISST, use field SST value
+# If field SST is more than 2 deg different from OISST, use OISST
+agg_stn_all_OISST$sstdiff <- agg_stn_all_OISST$sst - agg_stn_all_OISST$oisst
+summary(agg_stn_all_OISST$sstdiff)
+hist(agg_stn_all_OISST$sstdiff, breaks=seq(-24, 14, 1))
+nrow(agg_stn_all_OISST[!is.na(agg_stn_all_OISST$sstdiff) & abs(agg_stn_all_OISST$sstdiff) <=2.0,]) / 
+  nrow(agg_stn_all_OISST[!is.na(agg_stn_all_OISST$sstdiff),]) * 100
+# 85% of our observations with both values are within 2 deg.
+
+# Call new columns
+agg_stn_all_OISST$temp <- NA
+agg_stn_all_OISST$sstsource <- NA
+
+# Set temp to OISST when abs(sstdiff) >2
+# Retain source information
+for(i in 1:nrow(agg_stn_all_OISST)){
+  if(!is.na(agg_stn_all_OISST$sstdiff[i]) & abs(agg_stn_all_OISST$sstdiff[i]) <=2){
+    agg_stn_all_OISST$temp[i] <- agg_stn_all_OISST$sst[i]
+    agg_stn_all_OISST$sstsource[i] <- 'Field'
+  }
+  if(is.na(agg_stn_all_OISST$sstdiff[i]) | abs(agg_stn_all_OISST$sstdiff[i]) >2){
+    agg_stn_all_OISST$temp[i] <- agg_stn_all_OISST$oisst[i]
+    agg_stn_all_OISST$sstsource[i] <- 'OISST'
+  }
+  
+}
+# Check how often we are substituting field data for OISST
+barplot(t(prop.table(table(agg_stn_all_OISST$year, agg_stn_all_OISST$sstsource), margin=1)), legend=T)
+# Most years are more than 70% populated by field SsT data
+
+# Convert sf to df
+agg_stn_all_OISST <- sfheaders::sf_to_df(agg_stn_all_OISST, fill=T)
+agg_stn_all_OISST <- dplyr::select(agg_stn_all_OISST,
+                                   id, year, month, day, x, y,
+                                   Size_class, catch, fhours, depth, temp, sstsource)
+colnames(agg_stn_all_OISST) <- c('id', 'year', 'month', 'day', 'lon', 'lat',
+                                 'Size_class', 'catch', 'fhours', 'depth', 'sst',
+                                 'sstsource')
+
+# Save output
+#write.csv(agg_stn_all_OISST, row.names = F, 
+#          here('Data/Clean/AllYears_UpdatedSST.csv'))
---
+++ R_code/join_depth_noaabathy.R
@@ -0,0 +1,203 @@
+# Joins existing field station depth data and NOAA bathymetry depth data, tests
+# relationship between the two, visualizes.
+
+rm(list=ls())
+
+# Load libraries
+library(tidyverse)
+library(here)
+library(sf)
+library(raster)
+library(marmap)
+library(rgdal)
+# Negate function
+'%notin%' <- function(x,y)!('%in%'(x,y))
+# Set GGplot auto theme
+theme_set(theme(panel.grid.major = element_line(color='lightgray'),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(),
+                panel.border = element_rect(color='black', size=1, fill=NA),
+                legend.position = "bottom",
+                axis.text.x=element_text(size=12),
+                axis.text.y=element_text(size=12),
+                axis.title.x=element_text(size=14),
+                axis.title.y=element_text(size=14, angle=90, vjust=2),
+                plot.title=element_text(size=14, hjust = 0, vjust = 1.2),
+                plot.caption=element_text(hjust=0, face='italic', size=12)))
+
+# Load station data from OISST-SST merge
+stations <- read.csv(here('Data/Clean/AllYears_UpdatedSST.csv'))
+# Remember this is just station data, missing catch data from large category
+head(stations)
+
+# Load MARMAP bathymetry data
+Bathy <- getNOAA.bathy(lon1 = -80, lon2 = -60,
+                       lat1 = 30, lat2 = 50, 
+                       resolution = 1)
+# Convert to raster
+bathy_rast <- marmap::as.raster(Bathy)
+# Reproject to unprojected wgs84 lat-lon
+bathy_rast <- raster::projectRaster(bathy_rast, 
+                                    crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+# Clip raster to NWAtlantic shapefie
+nwat <- readOGR(here('Data/GIS/NWAtlantic.shp'))
+nwat <- spTransform(nwat, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+bathy_rast <- raster::mask(bathy_rast, nwat)
+plot(bathy_rast)
+
+# Clip out coast
+coast <- ecodata::coast
+coast <- st_transform(coast, crs="EPSG:4326")
+coast <- as(coast, 'Spatial')
+coast_rast <- raster::mask(bathy_rast, coast)
+plot(coast_rast)
+
+for(i in 1:length(bathy_rast@data@values)){
+  if(!is.na(bathy_rast@data@values[i]) & !is.na(coast_rast@data@values[i])){
+    bathy_rast@data@values[i] <- NA
+  }
+}
+bathy_rast@data@values[bathy_rast@data@values > 0] <- NA
+plot(bathy_rast)
+summary(bathy_rast@data@values)
+
+# Convert stations data to spatialpointsdataframe
+stations.sp <- SpatialPointsDataFrame(coords=as.matrix(cbind(stations$lon, stations$lat)),
+                                      data=stations,
+                                      proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+plot(bathy_rast)
+points(stations.sp, pch='.', cex=0.8)
+
+# Extract bathy at station points
+stations.sp$bathy <- extract(bathy_rast, stations.sp)
+stations.sp$bathy <- round(stations.sp$bathy, 1)
+
+# Reconvert to data frame
+stations <- as.data.frame(stations.sp)
+stations$coords.x1 <- NULL; stations$coords.x2 <- NULL
+  
+# Comparisons
+# Set sequence of years to plot
+yearstoplot <- seq(1993, 2021)
+
+# Plot comparisons, check for adhesion to 1:1 line
+for(i in 1:length(yearstoplot)){
+  # Set year
+  plotyr <- yearstoplot[i]
+  
+  # Plot OISST vs Field SST for that year
+  print(ggplot2::ggplot(stations[stations$year == plotyr,], 
+                        aes(x=depth, y=bathy)) +
+          geom_point(alpha=0.8, pch=16)+
+          geom_abline(intercept = 0, slope = 1) +
+          labs(x = "Field depth (m)",
+               y = "NOAA bathymetry (m)") +
+          theme_bw() +
+          ggtitle(paste("Depth differences:", plotyr, sep = " ")))
+}
+
+# Plot OISST vs Field SST for all years
+ggplot2::ggplot(stations, aes(x=depth, y=bathy, col=year)) +
+  geom_point(alpha=0.8, pch=16)+
+  geom_abline(intercept = 0, slope = 1) +
+  labs(x = "Field depth (m)",
+       y = "NOAA bathymetry (m)") +
+  theme_bw() 
+# Oh boy
+# Above 1:1 line== shallower NOAA bathymetry than field data
+# Below 1:1 line== shallower field data than NOAA bathymetry
+
+# Calculate difference
+stations$depthdif <- stations$depth - stations$bathy
+stations$depthdif[is.na(stations$depth) | is.na(stations$bathy)] <- NA
+hist(stations$depthdif, breaks=seq(-8840, 2810, 10),
+     xlim=c(-1000, 1000))
+# Huge prevalence of NOAA bathymetry underestimating field depth
+
+# Spatial plof of differences, all years
+stations.sf <- st_as_sf(stations, coords=c('lon', 'lat'), crs="EPSG:4326")
+coast <- ecodata::coast
+coast <- st_transform(coast, crs="EPSG:4326")
+ggplot2::ggplot() +
+  geom_sf(data = coast) +
+  geom_sf(data = stations.sf, 
+          aes(colour=depthdif)) +
+  coord_sf(xlim = c(-77, -65), 
+           ylim = c(35, 45)) + 
+  scale_color_gradient2(low = "blue",
+                        mid = "gray",
+                        high = "red",
+                        midpoint = 0,
+                        limits=c(-8900, 2200),
+                        na.value = "gray") +
+  theme_bw() +
+  #facet_wrap(~month) +
+  ggtitle(paste("Depth difference survey-NOAA bathy: All years"))
+
+# Spatial plot of differences, year by year
+# Set function to map SST difference
+yrmap <- function(mapyr){
+  coast <- ecodata::coast
+  coast <- st_transform(coast, crs="EPSG:4326")
+  ggplot2::ggplot() +
+    geom_sf(data = coast) +
+    geom_sf(data = stations.sf[stations.sf$year == mapyr,], 
+            aes(colour=depthdif)) +
+    coord_sf(xlim = c(-77, -65), 
+             ylim = c(35, 45)) + 
+    scale_color_gradient2(low = "blue",
+                          mid = "gray",
+                          high = "red",
+                          #midpoint = 0,
+                          limits=c(-8900, 2200),
+                          na.value = "gray") +
+    theme_bw() +
+    #facet_wrap(~month) +
+    ggtitle(paste("Depth difference survey-NOAA bathy:", mapyr, sep = " "))
+}
+
+# Plot SST difference in space for every year
+# Blue: NOAA shallower than field
+# Red: field shallower than NOAA
+for(mapyr in 1993:2021){
+  print(yrmap(mapyr)) 
+}
+
+# Executive decision: if field depth is within 215m deg of NOAA bathy, use field depth value
+# If field depth is more than 215m different from NOAA bathy, use NOAA bathy
+nrow(stations[!is.na(stations$depthdif) & abs(stations$depthdif) <= 170.0,]) / 
+  nrow(stations[!is.na(stations$depthdif),]) * 100
+# 90% of our observations with both values are within 170m
+
+# Call new columns
+stations$temp <- NA
+stations$depthsource <- NA
+
+# Set temp to OISST when abs(sstdiff) >2
+# Retain source information
+for(i in 1:nrow(stations)){
+  if(!is.na(stations$depthdif[i]) & abs(stations$depthdif[i]) <=170){
+    stations$temp[i] <- stations$depth[i]
+    stations$depthsource[i] <- 'Field'
+  }
+  if(is.na(stations$depthdif[i]) | abs(stations$depthdif[i]) >170){
+    stations$temp[i] <- stations$bathy[i]
+    stations$depthsource[i] <- 'NOAA'
+  }
+  
+}
+# Check how often we are substituting field data for OISST
+barplot(t(prop.table(table(stations$year, stations$depthsource), margin=1)), legend=T)
+abline(h=0.7, col='red')
+# Most years are more than 70% populated by field SsT data
+
+# Remove unnecessary columns
+stations <- dplyr::select(stations,
+                                   -depth, -bathy, -depthdif)
+colnames(stations) <- c(colnames(stations[1:11]), 'depth', 'depthsource')
+head(stations)
+
+# Save output
+write.csv(stations, row.names = F, 
+          here('Data/Clean/AllYears_UpdatedDepth.csv'))
