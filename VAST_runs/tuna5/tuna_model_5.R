d1deee8f74e21965f8dae50ab6a6881949b465ed
klankowi <klankowicz@gmri.org>
2023-04-11 22:17:01 -0400

    Rerun BFT VAST 1.5
    Rerun BFT VAST model 1.5
    
    Add partial effects plots

--- VAST_runs/tuna5/tuna_model_5.R
+++ VAST_runs/tuna5/tuna_model_5.R
@@ -28,6 +28,7 @@ theme_set(theme(panel.grid.major = element_line(color='lightgray'),
 
 # Load data
 dat <- read.csv(here('Data/Clean/BFT_US_catch_VASTdata.csv'))
+dat$depth <- dat$depth * -1
 dat$date <- as.POSIXct(paste0(dat$year, '-', dat$month, '-', dat$day),
                        format='%Y-%m-%d')
 
@@ -37,16 +38,26 @@ dat <- dat %>%
 
 # Add AMO and NAO
 nao <- read.csv(here('Data/Climate_Indices/daily_nao.csv'))
-nao <- nao %>% 
-  filter(year >= 1993 & year <=2021) %>% 
-  filter(month %in% seq(6, 10, 1))
-colnames(nao) <- c('year', 'month', 'day', 'nao')
 nao$date <- as.POSIXct(paste0(nao$year, '-',
                               nao$month, '-',
                               nao$day),
                        format="%Y-%m-%d")
+nao$nao.scale <- scale(nao$aao_index_cdas)
+ggplot() +
+  geom_point(data=nao[nao$year >=1993,], aes(x=date, y=nao.scale)) +
+  geom_smooth(data=nao[nao$year >=1993,], aes(x=date, y=nao.scale))
+
+nao <- nao %>% 
+  filter(year >= 1993 & year <=2021) %>% 
+  filter(month %in% seq(6, 10, 1))
+colnames(nao) <- c('year', 'month', 'day', 'nao', 'date', 'nao.scale')
+
 nao <- dplyr::select(nao, date, nao)
 head(nao)
+ggplot() +
+  geom_point(data=nao, aes(x=date, y=scale(nao))) +
+  geom_smooth(data=nao, aes(x=date, y=scale(nao)))
+
 dat <- merge(dat, nao, by=c('date'))
 
 amo <- read.csv(here('Data/Climate_Indices/monthly_amo.csv'))
@@ -191,3 +202,64 @@ save.image('tuna_5.RData')
 
 plot( fit )
 beep(8)
+
+#### Partial effects ####
+library(effects)  # Used to visualize covariate effects
+
+# Must add data-frames to global environment (hope to fix in future)
+covariate_data_full = fit$effects$covariate_data_full
+catchability_data_full = fit$effects$catchability_data_full
+
+# Plot 1st linear predictor, but could use `transformation` to apply link function
+form1 <- as.character(fit$X1_formula)
+form1 <- form1[[2]]
+form1 <- strsplit(form1, "+ ")
+form1 <- as.vector(form1[[1]])
+form1 <- form1[form1 != "+"]
+
+predlist.1 <- vector(mode='list', length(form1))
+
+for(i in 1:length(form1)){
+  usevar <- form1[i]
+  
+  predlist.1[[i]] = Effect.fit_model( fit,
+                           focal.predictors = paste0(usevar),
+                           which_formula = "X1",
+                           xlevels = 1000,
+                           category_number=1
+                           #transformation = list(link=identity, inverse=identity
+                           ) 
+  #)
+  
+}
+
+predlist.2 <- vector(mode='list', length(form1))
+
+for(i in 1:length(form1)){
+  usevar <- form1[i]
+  
+  predlist.2[[i]] = Effect.fit_model( fit,
+                                      focal.predictors = paste0(usevar),
+                                      which_formula = "X1",
+                                      xlevels = 1000,
+                                      category_number=2
+                                      #transformation = list(link=identity, inverse=identity
+  ) 
+  #)
+  
+}
+
+plot(predlist.1[[1]], xlab='scaled sst', main=NA)
+plot(predlist.2[[1]], xlab='scaled sst', main=NA)
+
+plot(predlist.1[[2]], xlab='scaled depth', main=NA)
+plot(predlist.2[[2]], xlab='scaled depth', main=NA)
+
+plot(predlist.1[[3]], xlab='scaled prey', main=NA)
+plot(predlist.2[[3]], xlab='scaled prey', main=NA)
+
+plot(predlist.1[[4]], xlab='scaled AMO', main=NA)
+plot(predlist.2[[4]], xlab='scaled AMO', main=NA)
+
+plot(predlist.1[[5]], xlab='scaled NAO', main=NA)
+plot(predlist.2[[5]], xlab='scaled NAO', main=NA)
