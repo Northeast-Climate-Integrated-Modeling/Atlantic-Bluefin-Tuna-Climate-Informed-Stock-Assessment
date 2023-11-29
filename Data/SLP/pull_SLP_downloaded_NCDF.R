922c7734aa28a41601e485aeececaa5490efb042
klankowi <klankowicz@gmri.org>
2023-11-29 11:31:48 -0500

    Add back non-sensitive data

---
+++ Data/SLP/pull_SLP_downloaded_NCDF.R
@@ -0,0 +1,55 @@
+# Adapted from code written by Kimberly Bastille and Sarah Gaichas
+# Theoretically should be able to download NetCDF files directly into R from
+# NOAA, but recently the files have downloaded as corrupt and not openable.
+# Manual downloading of yearly NetCDF files from 
+# (https://psl.noaa.gov/data/gridded/data.ncep.reanalysis.html) is slower
+# but more reliable. This code requires those NetCDF files to already be 
+# manually downloaded from NOAA's website and placed into the project directory.
+
+# Clear workspace
+rm(list=ls())
+
+# Load libraries
+library(ncdf4)
+library(raster)
+library(tidyverse)
+library(here)
+
+# Load functions
+source(here("R_code/utilities/nc_to_raster_func.R"))
+source(here("R_code/utilities/raster_to_sstdf_func.R"))
+
+# Set variable name to sea surface temperature
+varname <- 'slp'
+
+# Set period of years to work with
+years <- 1993:1993
+
+# Loop through years
+for(i in years) {
+  # Set file name of .nc file
+  name <- here('Data/SLP', paste0("slp.", i, ".nc"))
+  nc <- ncdf4::nc_open(name)
+  # Set file name of output .gri, .grd files
+  filename <- here::here("Data","SLP", 
+                         paste0("test_", i, ".grd"))
+  # Convert specified NetCDF file to raster
+  text <- knitr::knit_expand(text = "test_{{year}} <- nc_to_raster(nc = name, varname = varname)
+                                     raster::writeRaster(test_{{year}}, filename = filename, overwrite=TRUE)",
+                             year = i)
+  try(eval(parse(text = text)))
+  # Remove nc file to save space (will be deleted from directory)
+  unlink(name)
+  unlink(nc)
+  # Set file name of raster (.gri, .grd files)
+  name <- get(paste0("test_",i))
+  # Set file name of output .rds files
+  filename <- here::here("Data","SLP",
+                         paste0("slp", i, ".rds"))
+  # Save rasters as .rds files
+  text <- knitr::knit_expand(text = "sst{{year}} <- raster_to_sstdf(brick = name)
+                                     saveRDS(sst{{year}}, filename)",
+                             year = i)
+  try(eval(parse(text = text)))
+# Terminate outer loop
+}
